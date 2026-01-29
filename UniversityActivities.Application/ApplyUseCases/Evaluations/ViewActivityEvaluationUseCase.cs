6bc0aca44e9ba8d454fec4794f6c1d6c6a410084
mokh179 <mohammed.khaled17997@gmail.com>
2026-01-29 14:28:23 +0300

    Add

---
+++ UniversityActivities.Application/ApplyUseCases/Evaluations/ViewActivityEvaluationUseCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityActivities.Application.DTOs.Evaluation;
+using UniversityActivities.Application.Interfaces.Repositories.Activies.AdminActivies;
+using UniversityActivities.Application.UserCases.Activities.Student;
+
+namespace UniversityActivities.Application.ApplyUseCases.Evaluations
+{
+    public class ViewActivityEvaluationUseCase : IAdminActivityEvaluationQueryRepository
+    {
+        public Task<List<ActivityEvaluationCriteriaAverageDto>> GetCriteriaAveragesAsync(int activityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActivityEvaluationSummaryDto> GetSummaryAsync(int activityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
