2d34f219bd903962527bb075f07eac5fdd29b35a
mokh179 <mohammed.khaled17997@yahoo.com>
2026-02-09 21:10:49 +0200

    Add

--- UniversityActivities.Web/Areas/Admin/Pages/Activities/ActivityParticipationDetails.cshtml.cs
+++ UniversityActivities.Web/Areas/Admin/Pages/Activities/ActivityParticipationDetails.cshtml.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UniversityActivities.Application.Interfaces.Repositories.Activies.AdminActivies;
+using UniversityActivities.Web.Common;
 
 namespace UniversityActivities.Web.Areas.Admin.Pages.Activities
 {
-    public class ActivityParticipationDetailsModel : PageModel
+    public class ActivityParticipationDetailsModel : BaseModel
     {
+        private readonly IAdminActivityDetailsQuery _adminActivityDetailsQuery;
+         public ActivityParticipationDetailsModel(IAdminActivityDetailsQuery adminActivityDetailsQuery)
+        {
+                _adminActivityDetailsQuery = adminActivityDetailsQuery;
+        }
+       
         public void OnGet()
         {
         }
