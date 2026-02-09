968776d51f37f35b9d3031d50e3a880ae1d099b3
mokh179 <mohammed.khaled17997@yahoo.com>
2026-02-09 06:04:33 +0200

    Edit  Actiivty Page

--- UniversityActivities.Application/Mapping/Activities/ActivityProfile.cs
+++ UniversityActivities.Application/Mapping/Activities/ActivityProfile.cs
@@ -18,64 +18,66 @@ public class ActivityProfile : Profile
     private void MapCreateOrUpdate()
     {
         CreateMap<CreateOrUpdateActivityDto, Activity>()
-    // DTO ➜ Entity
-    .ForMember(dest => dest.StudentClubId,
-        opt => opt.MapFrom(src => src.ClubId))
+     // DTO ➜ Entity
+     .ForMember(dest => dest.StudentClubId,
+         opt => opt.MapFrom(src => src.ClubId))
 
-    .ForMember(dest => dest.StartDateTime,
-        opt => opt.MapFrom(src => src.StartDate))
+     .ForMember(dest => dest.StartDateTime,
+         opt => opt.MapFrom(src => src.StartDate))
 
-    .ForMember(dest => dest.EndDateTime,
-        opt => opt.MapFrom(src => src.EndDate))
+     .ForMember(dest => dest.EndDateTime,
+         opt => opt.MapFrom(src => src.EndDate))
 
-    .ForMember(dest => dest.ActivityTargetAudiences,
-        opt => opt.MapFrom(src =>
-            src.TargetAudienceIds.Select(id =>
-                new ActivityTargetAudience
-                {
-                    TargetAudienceId = id
-                })))
+     .ForMember(dest => dest.ActivityTargetAudiences,
+         opt => opt.MapFrom(src =>
+             src.TargetAudienceIds.Select(id =>
+                 new ActivityTargetAudience
+                 {
+                     TargetAudienceId = id
+                 })))
 
-    .ForMember(dest => dest.ActivityUsers,
-        opt => opt.MapFrom(src =>
-            src.Assignments.Select(a =>
-                new ActivityUser
-                {
-                    UserId = a.UserId,
-                    ActivityRoleId = a.ActivityRoleId
-                })))
+     .ForMember(dest => dest.ActivityUsers,
+         opt => opt.MapFrom(src =>
+             src.Assignments.Select(a =>
+                 new ActivityUser
+                 {
+                     UserId = a.UserId,
+                     ActivityRoleId = a.ActivityRoleId
+                 })))
 
-    .ForMember(dest => dest.Management.ManagementTypeId,opt => opt.MapFrom(src =>src.ManagementTypeId))
-    
-    .ForMember(dest => dest.ActivityStatusId, opt => opt.Ignore())
-    .ForMember(dest => dest.ActivityType, opt => opt.Ignore())
-    .ForMember(dest => dest.AttendanceMode, opt => opt.Ignore())
-    .ForMember(dest => dest.AttendanceScope, opt => opt.Ignore())
+     .ForMember(dest => dest.ActivityStatusId, opt => opt.Ignore())
+     .ForMember(dest => dest.ActivityType, opt => opt.Ignore())
+     .ForMember(dest => dest.AttendanceMode, opt => opt.Ignore())
+     .ForMember(dest => dest.AttendanceScope, opt => opt.Ignore())
 
-    // 🔁 Entity ➜ DTO
-    .ReverseMap()
+     // 🔁 Entity ➜ DTO
+     .ReverseMap()
 
-    .ForMember(dest => dest.ClubId,
-        opt => opt.MapFrom(src => src.StudentClubId))
+     .ForMember(dest => dest.ClubId,
+         opt => opt.MapFrom(src => src.StudentClubId))
 
-    .ForMember(dest => dest.StartDate,
-        opt => opt.MapFrom(src => src.StartDateTime))
+     .ForMember(dest => dest.StartDate,
+         opt => opt.MapFrom(src => src.StartDateTime))
 
-    .ForMember(dest => dest.EndDate,
-        opt => opt.MapFrom(src => src.EndDateTime))
+     .ForMember(dest => dest.EndDate,
+         opt => opt.MapFrom(src => src.EndDateTime))
 
-    .ForMember(dest => dest.TargetAudienceIds,
-        opt => opt.MapFrom(src =>
-            src.ActivityTargetAudiences
-               .Select(x => x.TargetAudienceId)))
+     .ForMember(dest => dest.ManagementTypeId,
+         opt => opt.MapFrom(src =>
+             src.Management.ManagementTypeId))
 
-    .ForMember(dest => dest.Assignments,
-        opt => opt.MapFrom(src =>
-            src.ActivityUsers.Select(u => new ActivityAssignmentDto
-            {
-                UserId = u.UserId,
-                ActivityRoleId = u.ActivityRoleId
-            })));
+     .ForMember(dest => dest.TargetAudienceIds,
+         opt => opt.MapFrom(src =>
+             src.ActivityTargetAudiences
+                 .Select(x => x.TargetAudienceId)))
+
+     .ForMember(dest => dest.Assignments,
+         opt => opt.MapFrom(src =>
+             src.ActivityUsers.Select(u => new ActivityAssignmentDto
+             {
+                 UserId = u.UserId,
+                 ActivityRoleId = u.ActivityRoleId
+             })));
 
     }
 }
