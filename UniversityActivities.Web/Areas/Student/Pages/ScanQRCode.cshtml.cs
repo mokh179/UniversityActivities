f852f9dfed3f04a5eb2b495d01044c476b58b0a0
mokh179 <mohammed.khaled17997@gmail.com>
2026-02-12 13:45:24 +0300

    Add Mark Attendance

--- UniversityActivities.Web/Areas/Student/Pages/ScanQRCode.cshtml.cs
+++ UniversityActivities.Web/Areas/Student/Pages/ScanQRCode.cshtml.cs
@@ -9,63 +9,86 @@ using UniversityActivities.Web.Common;
 namespace UniversityActivities.Web.Areas.Student.Pages
 {
     [AllowAnonymous]
+    [IgnoreAntiforgeryToken]
     public class ScanQRCodeModel : PageModel
     {
         private readonly IHandleActivityScanUseCase _scanUseCase;
+
+        public int ActivityId { get; set; }
 
         public ScanQRCodeModel(IHandleActivityScanUseCase scanUseCase)
         {
             _scanUseCase = scanUseCase;
         }
-        public async Task<IActionResult> OnGetAsync(
-        int activityId,
-        CancellationToken cancellationToken)
-        {
-            int? userId = null;
+        //public async Task<IActionResult> OnGetAsync(
+        //int activityId,
+        //CancellationToken cancellationToken)
+        //{
+        //    int? userId = null;
 
-            if (User.Identity?.IsAuthenticated == true)
-            {
-                var claim = User.FindFirst(ClaimTypes.NameIdentifier);
-                if (claim != null)
-                    userId = int.Parse(claim.Value);
-            }
+        //    if (User.Identity?.IsAuthenticated == true)
+        //    {
+        //        var claim = User.FindFirst(ClaimTypes.NameIdentifier);
+        //        if (claim != null)
+        //            userId = int.Parse(claim.Value);
+        //    }
+
+        //    var result = await _scanUseCase.ExecuteAsync(
+        //        activityId,
+        //        userId,
+        //        cancellationToken);
+
+        //    return HandleScanResult(result);
+        //}
+
+        //private IActionResult HandleScanResult(ActivityScanResult result)
+        //{
+        //    return result.Action switch
+        //    {
+        //        ScanAction.RedirectToLogin =>
+        //            Redirect($"/Login?returnUrl=/Activities/ScanQRCode/{result.ActivityId}"),
+
+        //        ScanAction.RegisterAndAttend =>
+        //            Redirect($"/Activities/Success/{result.ActivityId}"),
+
+        //        ScanAction.MarkAttendance =>
+        //            Redirect($"/Activities/Success/{result.ActivityId}"),
+
+        //        ScanAction.AlreadyAttended =>
+        //            Redirect($"/Activities/Already/{result.ActivityId}"),
+
+        //        ScanAction.RedirectToEvaluation =>
+        //            Redirect($"/Student/Evaluate/{result.ActivityId}"),
+
+        //        ScanAction.RedirectToCertificate =>
+        //            Redirect($"/Student/Certificate/{result.ActivityId}"),
+
+        //        ScanAction.ActivityNotStarted =>
+        //            Redirect($"/Activities/NotStarted/{result.ActivityId}"),
+
+        //        _ =>
+        //            Redirect($"/Activities/Closed/{result.ActivityId}")
+        //    };
+        //}
+
+        public async Task<IActionResult> OnGetProcessAsync(int activityId)
+        {
+            var userId = GetCurrentUserIdOrNull();
 
             var result = await _scanUseCase.ExecuteAsync(
                 activityId,
                 userId,
-                cancellationToken);
+                CancellationToken.None);
 
-            return HandleScanResult(result);
+            return new JsonResult(result);
         }
 
-        private IActionResult HandleScanResult(ActivityScanResult result)
+        private int? GetCurrentUserIdOrNull()
         {
-            return result.Action switch
-            {
-                ScanAction.RedirectToLogin =>
-                    Redirect($"/Login?returnUrl=/Activities/ScanQRCode/{result.ActivityId}"),
+            if (!User.Identity!.IsAuthenticated)
+                return null;
 
-                ScanAction.RegisterAndAttend =>
-                    Redirect($"/Activities/Success/{result.ActivityId}"),
-
-                ScanAction.MarkAttendance =>
-                    Redirect($"/Activities/Success/{result.ActivityId}"),
-
-                ScanAction.AlreadyAttended =>
-                    Redirect($"/Activities/Already/{result.ActivityId}"),
-
-                ScanAction.RedirectToEvaluation =>
-                    Redirect($"/Student/Evaluate/{result.ActivityId}"),
-
-                ScanAction.RedirectToCertificate =>
-                    Redirect($"/Student/Certificate/{result.ActivityId}"),
-
-                ScanAction.ActivityNotStarted =>
-                    Redirect($"/Activities/NotStarted/{result.ActivityId}"),
-
-                _ =>
-                    Redirect($"/Activities/Closed/{result.ActivityId}")
-            };
+            return int.Parse(User.FindFirst("uid")!.Value);
         }
     }
 }
